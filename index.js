f6f447adfc14a877c81cb746525434b5fccb66ed
Mihir Sasmal <mihir.sasmal@leicabiosystems.com>
2018-05-06 00:04:05 +0530

    added options check for prefight requests from browser. added response header aallow headers directly from request headers. removed unnecessary codes. working with both chrome and firefox

--- index.js
+++ index.js
@@ -2,31 +2,21 @@ var express = require('express');
 var request = require('request');
 
 var app = express();  
-var x=1;
 
 app.use('/proxy', function(req, res) {  
-    
-    console.log("printing req url"+ x++ );
-    console.log(req.url);
 
     var url = req.url.replace('/?url=','');
-    
-    console.log("printingurl "+ x );
-  console.log(url);
-  console.log("printing req header "+ x );
-  console.log(req.headers);
+
+  if(req.headers["access-control-request-headers"] != null)
+    res.header('Access-Control-Allow-Headers',`${req.headers["access-control-request-headers"]},*`);
+  else  
+    res.header('Access-Control-Allow-Headers','*');
 
   res.header('Access-Control-Allow-Origin' ,'*');
-  res.header('Access-Control-Allow-Headers','*');
-
-  req.pipe(
-      request.get({url:url, rejectUnauthorized: false}, function (error, response, body)
-             {
-              console.log('error:', error);
-              console.log('statusCode:',response.statusCode);
-              res.send(body);
-             })
-         );
+  
+  if(req.method == 'OPTIONS') res.send();
+  else 
+    req.pipe(request.get({url:url, rejectUnauthorized: false})).pipe(res);         
 });
 
 app.listen(process.env.PORT || 3000);  
